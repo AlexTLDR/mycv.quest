b6f0320c0bcf97ada212cccab58b770c0483cd1c
AlexTLDR <alex.badragan@protonmail.com>
2025-08-10 18:58:33 +0200

    reinitialized templui project

--- assets/migrations/000002_create_sessions_table.down.sql
+++
@@ -1,3 +0,0 @@
-{! if eq .Database "mysql" !}DROP INDEX idx_sessions_expiry ON sessions;{! else !}DROP INDEX idx_sessions_expiry;{! end !}
-
-DROP TABLE sessions;
--- assets/migrations/000002_create_sessions_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE sessions (
-	token TEXT PRIMARY KEY,
-	data BYTEA NOT NULL,
-	expiry TIMESTAMPTZ NOT NULL
-);
-
-CREATE INDEX idx_sessions_expiry ON sessions(expiry);
